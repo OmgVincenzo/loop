3539c38df2b10efc22f0bdd5dd01f821ff998f8b
Vincenzo <128050905+OmgVincenzo@users.noreply.github.com>
2023-04-18 03:35:57 +0600

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,10 @@
+import os
+
+message = input("Type Your Message: ")
+range_x = int(input("Message Loop Num: "))
+
+os.system('clear')
+
+for i in range(range_x):
+             
+      print(message)
